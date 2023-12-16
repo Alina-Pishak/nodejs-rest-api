11af7972d8c0baf72e393680faa88a0fb8315e6b
Alina-Pishak <alinapishak@icloud.com>
2023-12-16 20:01:20 +0200

    fix validateAvatar function

--- middlewares/validateAvatar.js
+++ middlewares/validateAvatar.js
@@ -3,6 +3,7 @@ const { HttpError } = require("../helpers");
 const validateAvatar = (req, res, next) => {
   if (req.file) {
     next();
+    return;
   }
   if (Object.keys(req.body).length < 1) {
     throw HttpError(400, "missing required avatar field");
